87251d62f41acf14c336c1e7de6a5a7c56e7a04c
kaxla <kaylas.email.address@gmail.com>
2014-04-05 18:33:14 -0700

    add blog scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Realportfolio::Application.routes.draw do
+  resources :blogs
+
   resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140406012821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
