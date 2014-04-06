ab504d6d05aa219b31c2d91b3f0fcc2e07314635
kaxla <kaylas.email.address@gmail.com>
2014-04-05 18:24:03 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem "pry-rails"
+  gem "pry-debugger"
+end
+
+group :test do
+  gem "minitest-rails-capybara"
+  gem "launchy"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Realportfolio::Application.config.secret_key_base = 'e396c2353363efd05a41930329c34aff45d4024ba39120e0fb562230ec6b4548fbd563ffab2af29a53eb7d9c44be923d47469d822a8383531a6d87f3d539a1bc'
