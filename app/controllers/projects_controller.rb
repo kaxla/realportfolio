e51a077cc1aa99be132b0bd75e05da22a5e74419
kaxla <kaylas.email.address@gmail.com>
2014-05-03 20:18:05 -0700

    image uploads work and display correctly

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :description, :image)
+      params.require(:project).permit(:title, :description, :project_image)
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,17 @@
 class Project < ActiveRecord::Base
   has_attached_file :project_image,
-                    storage: :s3,
-                    s3_credentials: {
-                      access_key_id: ENV['S3_ACCESS_KEY_ID'],
-                      secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-                      bucket: ENV['S3_BUCKET']
-                    },
-                    url: ":kaylamorrisonportfolio.s3-website-us-west-2.amazonaws.com"
+                    :storage => :s3,
+                    :s3_protocol => "https",
+                    :s3_host_alias => "s3-us-west-2.amazonaws.com",
+                    :path => "/image/:id/:filename",
+                    :bucket => ENV['S3_BUCKET'],
+                    :s3_credentials => {
+                      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+                      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
+                    }
   validates_attachment_file_name :project_image, :matches => [/png\Z/, /jpe?g\Z/]
 end
+# correct:
+# https://s3-us-west-2.amazonaws.com/kaylamorrisonportfolio/image/6/enhanced-buzz-511-1296591390-30.jpg
+#displaying:
+# http://s3.amazonaws.com/kaylamorrisonportfolio/image/3/enhanced-buzz-517-1296592131-31.jpg?1399170852
