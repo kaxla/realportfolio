075b48d3835f6d33e62fc7bce6f8e342f65c6291
kaxla <kaylas.email.address@gmail.com>
2014-06-27 11:03:58 -0700

    add authorization to blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /blogs
   # GET /blogs.json
