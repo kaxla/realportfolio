cecca25491fe09be9003d05b8f3adbc40c089ea0
kaxla <kaylas.email.address@gmail.com>
2014-09-09 11:38:10 -0700

    add new relic

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{192.241.232.100}
+role :web, %w{192.241.232.100}
+role :db,  %w{192.241.232.100}
 
 
 # Extended Server Syntax
