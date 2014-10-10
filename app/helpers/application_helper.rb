1f1120298c14528f609b28287242bcf5d98f80cd
kaxla <kaylas.email.address@gmail.com>
2014-10-10 11:33:04 -0700

    delete commented out code I wasn't using anymore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,22 @@
 module ApplicationHelper
-  # def markdown(text)
-  #   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, fenced_code_blocks: true, disable_indented_code_blocks: true)
-  #   return markdown.render(text).html_safe
-
-  # end
-
   class CodeRayify < Redcarpet::Render::HTML
-  def block_code(code, language)
-    CodeRay.scan(code, language).div
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
   end
-end
 
-def markdown(text)
-  coderayified = CodeRayify.new(:filter_html => true,
-                                :hard_wrap => true)
-  options = {
-    :fenced_code_blocks => true,
-    :no_intra_emphasis => true,
-    :autolink => true,
-    :strikethrough => true,
-    :lax_html_blocks => true,
-    :superscript => true
-  }
-  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-  markdown_to_html.render(text).html_safe
-end
+  def markdown(text)
+    coderayified = CodeRayify.new(:filter_html => true,
+                                  :hard_wrap => true)
+    options = {
+      :fenced_code_blocks => true,
+      :no_intra_emphasis => true,
+      :autolink => true,
+      :strikethrough => true,
+      :lax_html_blocks => true,
+      :superscript => true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
