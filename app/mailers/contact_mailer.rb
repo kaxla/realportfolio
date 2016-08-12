8d930ce3e8883dd198e391c23e6d4564b6cc2b5c
kaxla <kaylas.email.address@gmail.com>
2016-08-12 15:23:59 -0700

    add from

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,10 @@
 class ContactMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
-    mail(to: ENV['EMAIL'], subject: 'Website email')
+    mail(
+      to: ENV['EMAIL'],
+      from: ENV['FROM_EMAIL'],
+      subject: 'Website email'
+    )
   end
 end
